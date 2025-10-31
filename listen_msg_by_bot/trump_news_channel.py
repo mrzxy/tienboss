8d80e866ff2dda1b021da92a7491ddc7717560d1
zxy <zxy_fd139@163.com>
2025-10-31 13:34:42 +0800

    add webhook

--- listen_msg_by_bot/trump_news_channel.py
+++ listen_msg_by_bot/trump_news_channel.py
@@ -1,6 +1,5 @@
 from config import get_config
 from chat import send_chat_request, send_msg_by_webhook, send_chat_request_by_trump_news, send_msg_by_webhook_sync, extract_image_urls
-from dc_history import insert_message_if_not_exists, update_last_message_id, generate_content_md5,search_message_by_content_md5
 import datetime
 from helper import get_logger,contains_chinese
 
@@ -28,6 +27,8 @@ webhook_map = {
     'Reuters': 'https://discord.com/api/webhooks/1421683254271934505/Um3jzKrn0HNKCPGBefV-oBikFmG8onfL2VIrTmDbmSxULMik2vzbkrfA10uFylxWFkI9',
     '*Walter Bloomberg': 'https://discord.com/api/webhooks/1421683391723470951/B6FK1xUM4bl4nl43CgGEKLcQSQ6sRJAz_AunjGGahj9r6CXMhraMgk_XH27AShaoqdES',
     'Wall St Engine': 'https://discord.com/api/webhooks/1421683654198825052/G7N3n-qa0aYNtajGFE58EdK08_CcQeQ3i6SkthC8EkcLhVT4Ic--MW2zDjdOPkJo5OSf',
+    # TODO
+    'FinancialJuice': 'https://discord.com/api/webhooks/1433668737969688617/RP7dTRf2BX1p81fAuWGJfbZE_qK-j4-RVhIRqEaW26VisOFZcrVj8D2fNSLTb4ewK9Fw',
     'qiyuhook': 'https://discord.com/api/webhooks/1421750366483251232/9g_IvTelfhqj8uP-IAxIAEcQt94ivQCM3AeTqhXESiXDGpAbfbwNZW8l23FJXPvtVolo'
 }
 
